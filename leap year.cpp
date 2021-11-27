4c46fb2395a1c8cf6e61519faef533bd6df3e3d2
Avinash7223 <95042051+Avinash7223@users.noreply.github.com>
2021-11-27 18:08:43 +0530

    Update leap year.cpp

--- leap year.cpp
+++ leap year.cpp
@@ -1,16 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
+int main() {
+   int year;
+  
+   scanf("%d", &year);
 
-int main()
-
-{ 
-	int year;
-	scanf("%d",&year);
-	if(year%4==0)
-	{
-		printf("it is leap year");
-	}
-	else
-	{ 
-		printf("it is not leap year");
-	}
-}
+  
+   if (year % 400 == 0) {
+      printf("Leap year");
+   }
+  
+   else if (year % 100 == 0) {
+      	printf("Non leap year");
+   }
+ 
+   else if (year % 4 == 0) {
+      printf("Leap year");
+   }
+  
+   else {
+      	printf("Non leap year");
+   }
