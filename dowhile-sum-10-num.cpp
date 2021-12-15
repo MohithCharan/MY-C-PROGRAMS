10db42fa947026b0406e6534a37cfb1b937a7617
Avinash7223 <95042051+Avinash7223@users.noreply.github.com>
2021-12-15 11:57:37 +0530

    Update dowhile-sum-10-num.cpp

--- dowhile-sum-10-num.cpp
+++ dowhile-sum-10-num.cpp
@@ -1,23 +1,16 @@
-/*6. 10 numbers are given in the input. Read them and print their sum. Use as few variables as you
-can.*/
- 
- #include<stdio.h>
+// 10 numbers are given in the input. Read them and print their sum. Use as few variables as you can.//
+
+#include<stdio.h>
 int main()
-{ int a,b,c;
-   scanf("%d",&a);
-  a=a;
-  b=a+9;
-   if (a<=b)
-   { 
- do
-   {
-    c=c+a;
-    a=a+1;
-	}while(a<=b);
-   }
-   printf("\nthe sum is:%d",c);
-
-
-   
-  return 0;
+{ int i,n,num,sum;
+ n=10;
+ i=1;
+sum=0;
+for(i=1;i<=10;i++)
+{ 
+    scanf("%d",&num);
+sum=sum+num;
+ 
+ }
+ printf("the sum of all numbers is: %d",sum);
 }
